0aee53d3b8f1c2fa5a12e52bdf61c4b10aac2a1b
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-08 14:34:48 +0530

    Added validity

--- src/js_assignment_product.js
+++ src/js_assignment_product.js
@@ -1,40 +1,55 @@
 //javascipt
-var lists=[];
-function productList(){
-    var product=insertJson();
+var lists = [];
+function productList() {
+  var product = insertJson();
+  if (product!= -1){
+    console.log("Entering the push array part");
     lists.push(product);
-    console.log(lists);
-    table =document.getElementById("table");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    for (var i=0; i<lists.length; i++){
-        cell1.innerHTML = lists[i].pro_id;
-        cell2.innerHTML = lists[i].pro_name;
-        cell3.innerHTML = lists[i].pro_price;
+  }
+  
+  console.log(lists);
+  display(lists);
+}
+
+function insertJson() {
+  var id = document.getElementById("productId").value;
+  var name = document.getElementById("productName").value;
+  var price = document.getElementById("productPrice").value;
+  var check = validation(id, lists);
+  if (!check) {
+    var data = { pro_id: id, pro_name: name, pro_price: price };
+    return data;
+  } else {
+    console.log("yes id exists");
+    alert("Product with this ID is already added in List");
+    return -1;
+  }
+}
+
+function validation(id, lists) {
+  console.log("Hello id " + id);
+  console.log(lists);
+  for (i = 0; i < lists.length; i++) {
+    if (id == lists[i].pro_id) {
+      return true;
     }
+  }
 }
 
-function insertJson(){
-    var id=document.getElementById("productId").value;
-    var name=document.getElementById("productName").value;
-    var price=document.getElementById("productPrice").value;
-    return {
-        "pro_id":id,
-        "pro_name":name,
-        "pro_price":price
-    }
+function display(lists) {
+  console.log("Entering display function");
+  var table =
+    "<table><tr><th>Product ID</th><th>Product Name</th><th>Product Price</tj></tr>";
+  for (var i = 0; i < lists.length; i++) {
+    table +=
+      "<tr><td>" +
+      lists[i].pro_id +
+      "</td><td>" +
+      lists[i].pro_name +
+      "</td><td>" +
+      lists[i].pro_price +
+      "</td></tr>";
+  }
+  table += "</table>";
+  document.getElementById("table").innerHTML = table;
 }
-
-
-/*function display(lists){
-    document.getElementById("id").innerHTML=id;
-    document.getElementById("name").innerHTML=name;
-    document.getElementById("price").innerHTML=price;
-}
-
-function addList(id,name,price){
-    document.getElementById("id").innerHTML=id;
-    document.getElementById("name").innerHTML=name;
-    document.getElementById("price").innerHTML=price;*/
